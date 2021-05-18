db8a508d6c21a3c716762a5690804f8fe396fb55
Paulo <paulo_sanches2012@hotmail.com>
2021-05-18 14:46:52 -0300

    criacao da populacao inicial

--- algoritmo-genetico/main.py
+++ algoritmo-genetico/main.py
@@ -1,29 +1,29 @@
 import numpy as np
 import random
 
-def individuo(): #Individuo inicial com tamanho 1x40
-    individuoInicial = np.random.choice([0], size=(1, 40))
-    aleatorio = random.randrange(0, 2)
+def populacao_inicial(): #População de individuos iniciais com valores randômicos
+    populacao = []
 
-    return individuoInicial
-    # aleatorio = random.randrange(0, 2)
-    # for i in individuoInicial:
-    #     individuoInicial[i] = aleatorio
-    # return individuoInicial
+    # Está criando uma populacao com 10 individuos
+    for i in range(10):
+        individuo = ''
+        for j in range(40):
+            # Cada individuo está com 4 respostas
+            possiveisrespostas = ['0001', '0010', '0100', '1000']
+            respostasorteada = random.randrange(0, 4)
+            individuo += possiveisrespostas[respostasorteada]
+        populacao.append(individuo)
 
-# print("Individuo Inicial", individuo())
-
-def populacao_inicial(): #População de individuos iniciais com valores randômicos, tamanho 10x40
-    populacao = np.random.choice([0], size=(4, 10))
-
-    for i in populacao:
-        aleatorio = random.randrange(0, 2)
-        a = i[:4]
-        a += aleatorio
-        # print(i)
-        print("valor dos individuos do array", a)
-    # return populacao
+    return  populacao
 
 print(populacao_inicial())
+
+
+# def teste():
+#     populacao = np.zeros((10, 5))
+#     a = np.random.choice([0], size=(4, 10))
+#     print(a)
+#     return  populacao
+# print(teste())
 
 # print(random.randrange(0,2)) #gera valor aleatorio 0 e 1
