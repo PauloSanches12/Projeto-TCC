89f75579e0641f999df15bf1148d9593f803e2f5
Paulo <paulo_sanches2012@hotmail.com>
2021-07-20 00:08:52 -0300

    created function mutation

--- algoritmo-genetico/ag.py
+++ algoritmo-genetico/ag.py
@@ -70,15 +70,16 @@ def criapesos():
 def criaPopulacao():
     # Está criando uma populacao com 10 individuos
     populacao = []
+    possiveisRespostas = ['0001', '0010', '0100', '1000']
     for i in range(10):
         individuo = ''  # observa que individuo é uma string
         for j in range(6):  # vai trocar isso por 20 depois
             # Cada individuo está com respostas para 6 questões
-            possiveisrespostas = ['0001', '0010', '0100', '1000']
+
             # 0 a opção não foi escolhida, 1 a opção foi escolhida
-            respostasorteada = random.randrange(0, 4)
-            individuo += possiveisrespostas[respostasorteada]
-        print(individuo)
+            respostaSorteada = random.randrange(0, 4)
+            individuo += possiveisRespostas[respostaSorteada]
+        # print(individuo)
         populacao.append(individuo)
     return populacao
 
@@ -109,6 +110,69 @@ def calculaFit(populacao, dicpeso):
         questao = 0
         for j in range(0, len(i), 4):  # pega as respostas de cada indivíduo por questão
             questao += 1
+            #print('Questao:',questao)
+            #print(i[j:j+4]) #a resposta em binário
+            decimal = int(i[j:j + 4], 2)
+            # print(decimal) #a resposta em decimal: ou 1 ou 2 ou 4 ou 8
+            resposta = int(str(questao) + str(decimal))  # junta a questão com a resposta em decimal
+            #print(dic[resposta])
+
+        if dic[resposta] in dicpeso:  # caso nenhum dos entrevistados tenha escolhido alguma opcao, esta não estará no dic de dicpeso.
+            # aqui evita o erro de procurar por algo que não há
+            dicFit[individuo] = dicFit.get(individuo, 0) + dicpeso[dic[resposta]]
+
+        if individuo not in dicFit:  # caso o individuo tenha respondido só opções que não estão em dicpeso, então seu fitness será zero
+            dicFit[individuo] = 0
+        individuo += 1
+    print(dicFit)
+    return dicFit
+
+
+def seleciona(dicfit):
+    listaSelecionados = []
+    for i in sorted(dicfit, key=dicfit.get)[
+             4:]:  # ordena o dicionário pelo fit e mostra só os 6 melhores - essa quantidade mudará para 10
+        # print(i,round(dicfit[i],1))
+        listaSelecionados.append(i)
+    # print('SELECIONADOS:',listaSelecionados)
+    return listaSelecionados
+
+
+def criaNovaPop(populacao, selecionados):
+    novapop = []
+    for i in selecionados[0:4]:  # adiciona os 4 melhores na nova população
+        novapop.append(populacao[i])
+    return novapop
+
+
+def cruzamento(populacao, selecionados, novapop):
+    for i in range(0, 5, 2):  # pega os 6 individuos, de 2 em dois porque o selecionado[i] vai cruzar com o selecionado[i+1] e vice-versa
+        # print(selecionados[i])
+        # print(selecionados[i+1])
+        ind1 = populacao[selecionados[i]]
+        ind2 = populacao[selecionados[i + 1]]
+        filho1 = ''
+        filho2 = ''
+        for j in range(0, len(ind1) - 4,
+                       8):  # pega as respostas pares de de individuo1 por questão e concatena com as impares de individuo2
+            filho1 += ind1[j:j + 4] + ind2[j + 4:j + 8]
+            filho2 += ind2[j:j + 4] + ind1[j + 4:j + 8]
+        # print('pai1:',ind1)
+        # print('pai2:',ind2)
+        # print('filho1:',filho1)
+        # print('filho2:',filho2)  #recomendo copiar no papel pai1 e pai 2 e ver o cruzamento (conforme está escrito na sua monografia)
+        novapop.append(filho1)
+        novapop.append(filho2)
+    return novapop
+
+
+def addFit(populacao, dicpeso):
+    individuo = 0
+    dicFit = {}
+    for i in populacao:
+        questao = 0
+        for j in range(0, len(i), 4):  # pega as respostas de cada indivíduo por questão
+            questao += 1
             # print('Questao:',questao)
             # print(i[j:j+4]) #a resposta em binário
             decimal = int(i[j:j + 4], 2)
@@ -119,43 +183,39 @@ def calculaFit(populacao, dicpeso):
         if dic[
             resposta] in dicpeso:  # caso nenhum dos entrevistados tenha escolhido alguma opcao, esta não estará no dic de dicpeso.
             # aqui evita o erro de procurar por algo que não há
-            dicFit[individuo] = dicFit.get(individuo, 0) + dicpeso[dic[resposta]]
+            dicFit[i] = dicFit.get(i, 0) + dicpeso[dic[resposta]]
 
-        if individuo not in dicFit:  # caso o individuo tenha respondido só opções que não estão em dicpeso, então seu fitness será zero
-            dicFit[individuo] = 0
+        if i not in dicFit:  # caso o individuo tenha respondido só opções que não estão em dicpeso, então seu fitness será zero
+            dicFit[i] = 0
         individuo += 1
     return dicFit
 
 
-def seleciona(dicfit):
-    listaSelecionados = []
-    for i in sorted(dicfit, key=dicfit.get)[
-             4:]:  # ordena o dicionário pelo fit e mostra só os 6 melhores - essa quantidade mudará para 10
-        print(i, round(dicfit[i], 1))
-        listaSelecionados.append(i)
-    print('SELECIONADOS:', listaSelecionados)
-    return listaSelecionados
+def mutacao(populacao):
+    possiveisRespostas = ['0001', '0010', '0100', '1000']
+    for i in range(4):
+        individuo = random.randrange(0, 10)
+        questao = random.randrange(0, 20, 4)  # vai trocar o 24 por 156 depois
 
+        # print('individuo:',individuo)
+        # print('questao:',questao)
+        # print('**',populacao[individuo][0:questao]) #antes da resposta sorteada
+        # print('**',populacao[individuo][questao+4:]) #depois da resposta sorteada
+        # print('**',populacao[individuo][questao:questao+4]) #a resposta sorteada
 
-def cruzamento(populacao, selecionados):
-    novapop = []
-    for i in range(0, 5,
-                   2):  # pega os 6 individuos, de 2 em dois porque o selecionado[i] vai cruzar com o selecionado[i+1] e vice-versa
-        print(selecionados[i])
-        print(selecionados[i + 1])
-        ind1 = populacao[selecionados[i]]
-        ind2 = populacao[selecionados[i + 1]]
-        filho1 = ''
-        filho2 = ''
-        for j in range(0, len(ind1) - 4,
-                       8):  # pega as respostas pares de de individuo1 por questão e concatena com as impares de individuo2
-            filho1 += ind1[j:j + 4] + ind2[j + 4:j + 8]
-            filho2 += ind2[j:j + 4] + ind1[j + 4:j + 8]
-        print('pai1:', ind1)
-        print('pai2:', ind2)
-        print('filho1:', filho1)
-        print('filho2:',
-              filho2)  # recomendo copiar no papel pai1 e pai 2 e ver o cruzamento (conforme está escrito na sua monografia)
-        novapop.append(filho1)
-        novapop.append(filho2)
-    return novapop
+        # print('Antes:',populacao[individuo])
+        respostaAntiga = populacao[individuo][questao:questao + 4]
+        troca = random.randrange(0, 4)
+
+        novaResposta = possiveisRespostas[troca]
+        # print(novaResposta)
+        while respostaAntiga == novaResposta:
+            troca = random.randrange(0, 4)
+            novaResposta = possiveisRespostas[troca]
+            # print(novaResposta)
+
+        # atualiza
+        populacao[individuo] = populacao[individuo][0:questao] + novaResposta + populacao[individuo][questao + 4:]
+
+        # print('Depois',populacao[individuo])
+    return populacao
--- algoritmo-genetico/main.py
+++ algoritmo-genetico/main.py
@@ -3,7 +3,22 @@ populacao = algoritmo.criaPopulacao()
 print(populacao)
 
 dicpeso = algoritmo.criapesos()
+
+populacao = algoritmo.criaPopulacao()
+
+populacaoComFit = algoritmo.addFit(populacao,dicpeso)
+print('População original')
+for i in sorted(populacaoComFit,key=populacaoComFit.get):
+    print(i,round(populacaoComFit[i],1))
+
 dicfitness = algoritmo.calculaFit(populacao,dicpeso)
 selecionados = algoritmo.seleciona(dicfitness)
-novapop= algoritmo.cruzamento(populacao,selecionados)
-print(novapop)
+novapop = algoritmo.criaNovaPop(populacao, selecionados)
+novapop= algoritmo.cruzamento(populacao,selecionados,novapop)
+#print(novapop)
+novapop = algoritmo.mutacao(novapop)
+#print(novapop)
+populacaoComFit = algoritmo.addFit(novapop,dicpeso)
+print('População nova')
+for i in sorted(populacaoComFit,key=populacaoComFit.get):
+    print(i,round(populacaoComFit[i],1))
